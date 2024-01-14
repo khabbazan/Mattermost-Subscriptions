3afb335c45de6fed80b4c4556c505755db4e5dff
khabbazan <ar.khabbazan@gmail.com>
2024-01-14 15:54:41 +0330

    add systemd files to project

--- mattermostsub/asgi.py
+++ mattermostsub/asgi.py
@@ -6,11 +6,13 @@ from channels.routing import URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
+django_asgi_app = get_asgi_application()
+
 from mattermostsub.consumers import MyGraphqlWsConsumer
 from mattermostsub.middlewares import JWTwsAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mattermostsub.settings")
 
 application = ProtocolTypeRouter(
-    {"http": get_asgi_application(), "websocket": JWTwsAuthMiddleware(AuthMiddlewareStack(URLRouter([path("ws/graphql/", MyGraphqlWsConsumer.as_asgi())])))}
+    {"http": django_asgi_app, "websocket": JWTwsAuthMiddleware(AuthMiddlewareStack(URLRouter([path("ws/graphql/", MyGraphqlWsConsumer.as_asgi())])))}
 )
